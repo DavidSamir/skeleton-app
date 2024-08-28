908fc30c05a4ed34a30b3006c2398df5938b6b58
David <David.Samir.WD@gmail.com>
2024-08-28 23:19:27 +0400

    test the new migration

--- timesheet_management/database/migrations/2024_08_27_215637_create_project_and_user_pivot_table.php
+++ timesheet_management/database/migrations/2024_08_27_215637_create_project_and_user_pivot_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateProjectsAndRelatedTables extends Migration
+class CreateProjectAndUserPivotTable extends Migration
 {
     public function up()
     {
