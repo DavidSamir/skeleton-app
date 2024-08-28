14398b0ff1768ff86ec1755127a7fbeb26af6f6c
David <David.Samir.WD@gmail.com>
2024-08-28 23:15:41 +0400

    update dataabse  struc

--- timesheet_management/database/migrations/2024_08_27_215636_create_users_table.php
+++ timesheet_management/database/migrations/2024_08_27_215636_create_users_table.php
@@ -6,26 +6,21 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     */
     public function up(): void
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('first_name');
             $table->string('last_name');
-            $table->date('date_of_birth');
-            $table->string('gender');
+            $table->date('date_of_birth')->nullable();
+            $table->string('gender')->nullable(); 
             $table->string('email')->unique();
             $table->string('password');
             $table->timestamps();
+            $table->softDeletes();
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
     public function down(): void
     {
         Schema::dropIfExists('users');
