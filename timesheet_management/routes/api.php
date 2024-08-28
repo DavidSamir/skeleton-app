1b26086479c09b28bda02acd00608e4c10311c88
David <David.Samir.WD@gmail.com>
2024-08-28 12:57:18 +0400

    move all the api endpoints to seprate file

---
+++ timesheet_management/routes/api.php
@@ -0,0 +1,17 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\ProjectController;
+use App\Http\Controllers\TimesheetController;
+use App\Http\Controllers\AuthController;
+
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
+Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth:sanctum');
+
+Route::middleware('auth:sanctum')->group(function () {
+    Route::apiResource('users', UserController::class);
+    Route::apiResource('projects', ProjectController::class);
+    Route::apiResource('timesheets', TimesheetController::class);
+});
--- timesheet_management/routes/web.php
+++ timesheet_management/routes/web.php
@@ -1,23 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\UserController;
-use App\Http\Controllers\ProjectController;
-use App\Http\Controllers\TimesheetController;
-use App\Http\Controllers\AuthController;
-
 
 Route::get('/', function () {
     return view('welcome');
-});
-
-
-Route::post('/register', [AuthController::class, 'register']);
-Route::post('/login', [AuthController::class, 'login']);
-Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth:sanctum');
-
-Route::middleware('auth:sanctum')->group(function () {
-    Route::apiResource('users', UserController::class);
-    Route::apiResource('projects', ProjectController::class);
-    Route::apiResource('timesheets', TimesheetController::class);
 });
