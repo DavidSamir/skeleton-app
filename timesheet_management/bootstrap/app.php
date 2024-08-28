cbd06c2655c02790ea5c37f1fc3f7cee67e4676a
David <David.Samir.WD@gmail.com>
2024-08-28 16:29:43 +0400

    init auth handler

--- timesheet_management/bootstrap/app.php
+++ timesheet_management/bootstrap/app.php
@@ -3,11 +3,12 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Illuminate\Auth\AuthenticationException;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
-        web: __DIR__ . '/../routes/web.php',
-        commands: __DIR__ . '/../routes/console.php',
+        web: __DIR__.'/../routes/web.php',
+        commands: __DIR__.'/../routes/console.php',
         health: '/up',
         api: __DIR__ . '/../routes/api.php'
     )
@@ -15,5 +16,9 @@ return Application::configure(basePath: dirname(__DIR__))
         //
     })
     ->withExceptions(function (Exceptions $exceptions) {
-        //
+
+        $exceptions->renderable(function (AuthenticationException $e, $request) {
+            return response()->json(['error' => 'Unauthorized'], 401);
+        });
+
     })->create();
